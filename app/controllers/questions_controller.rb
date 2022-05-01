546a55802e0994914ce072d869e75607a917bf94
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-05-01 12:26:00 +0300

    Add errorsMessages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,6 +17,7 @@ class QuestionsController < ApplicationController
         @question = Question.new question_params
 
         if @question.save 
+            flash[:success] = "Question created!"
             redirect_to questions_path
         else 
             render :new
@@ -31,6 +32,7 @@ class QuestionsController < ApplicationController
     def update 
         @question = Question.find_by id: params[:id]
         if @question.update question_params
+            flash[:success] = "Question updated!"
             redirect_to questions_path
         else 
             render :edit 
@@ -40,6 +42,7 @@ class QuestionsController < ApplicationController
     def destroy
         @question = Question.find_by id: params[:id]
         @question.destroy
+        flash[:success] = "Question deleted!"
         redirect_to questions_path
     end 
 
